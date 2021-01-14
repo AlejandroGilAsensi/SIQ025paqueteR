58806ec1f55c8d835e378aa3f3b7c404677f70f6
Alejandro Gil <al403052@uji.es>
2021-01-14 20:48:07 +0100

    Commit inicial

---
+++ R/suma.R
@@ -0,0 +1,18 @@
+#' Suma entre dos números
+#'
+#' Esta función devuelve la suma entre dos números.
+#'
+#' @param a Primer número
+#' @param b Segundo número
+#'
+#' @return La suma de a y b
+#' @export
+#'
+#' @examples
+#' suma(5,3)
+#'
+#' @seealso \link{resta}
+#'
+suma = function(a,b){
+  return(a + b)
+}
