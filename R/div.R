89da383a413a8a95aff966eb347c393031d55acf
Alejandro Gil <al403052@uji.es>
2021-01-15 18:26:38 +0100

    Se han añadido las funcinoes de mult y div

---
+++ R/div.R
@@ -0,0 +1,22 @@
+#' División entre dos números
+#'
+#' Esta función devuelve la división entre dos números.
+#'
+#' @param a Primer número
+#' @param b Segundo número
+#'
+#' @return La división entre a y b
+#' @export
+#'
+#' @examples
+#' div(3,7.2)
+#'
+#' @seealso \link{mult}
+#'
+div = function(a,b){
+  if (b==0){
+    stop("El segundo numero no puede ser 0")
+  }
+
+  return(a / b)
+}
---
+++ R/mult.R
@@ -0,0 +1,18 @@
+#' Multiplicación entre dos números
+#'
+#' Esta función devuelve la multiplicación entre dos números.
+#'
+#' @param a Primer número
+#' @param b Segundo número
+#'
+#' @return El producto entre a y b
+#' @export
+#'
+#' @examples
+#' mult(4,2.3)
+#'
+#' @seealso \link{div}
+#'
+mult = function(a,b){
+  return(a * b)
+}
